78dddedb84b898ca6bb659093011d024f39cb6ad
floy <floy-dev@mail.ru>
2022-07-14 11:48:59 +0300

    fast commit

--- dprod.sh
+++ dprod.sh
@@ -1,1 +1,5 @@
-docker compose -f docker-compose.yaml -f docker-compose.prod.yaml up --build
+if test -f "./.env.local"; then
+    docker compose --env-file .env.local -f docker-compose.yaml -f docker-compose.prod.yaml up --build -d
+else
+    docker compose -f docker-compose.yaml -f docker-compose.prod.yaml up --build -d
+fi
