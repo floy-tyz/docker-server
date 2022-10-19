388e2a26d5e391a04645a61662868e834e58f1f0
Floy <floy-dev@mail.ru>
2022-10-19 16:13:34 +0300

    fast commit

--- prod.sh
+++ prod.sh
@@ -1,5 +1,13 @@
-if test -f "./.env.local"; then
+WARNING='\033[1;33m'
+NC='\033[0m'
+
+ENV_ABSOLUTE_PATH=$(realpath "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.env.local")
+
+if test -f "$ENV_ABSOLUTE_PATH"; then
     docker compose --env-file .env.local -f docker-compose.yaml -f docker-compose.prod.yaml up --build -d
 else
-    docker compose -f docker-compose.yaml -f docker-compose.prod.yaml up --build -d
+    echo ""
+    printf "%b%s do not exist%b\n" "$WARNING" "$ENV_ABSOLUTE_PATH" "$NC"
+    echo ""
+    exit
 fi
